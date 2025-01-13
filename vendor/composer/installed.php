f501df494f4428aca33d11fafb84826f338f38dc
kablm <Kaderbelem428@gmail.com>
2025-01-13 11:01:56 +0100

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kader/portfolio-kader',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '36a1c3d387f98bd231d1350f3a149e7bd6467f30',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kader/portfolio-kader' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '36a1c3d387f98bd231d1350f3a149e7bd6467f30',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
